104cbfbd31f134087663cc6a97b5ef4fd94b6bb4
mohandasss <gamermohan39@gmail.com>
2025-02-07 23:57:05 +0530

    LensBox Project added and Resume Upadted

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,54 +1,64 @@
-import styles from './ProjectsStyles.module.css';
-import viberr from '../../assets/hotelwala..png';
-import freshBurger from '../../assets/conact.png';
-import hipsster from '../../assets/quizzy.png';
-import movieapp from '../../assets/folio.png';
-import ProjectCard from '../../common/ProjectCard';
-import Devshare from "../../assets/DevCaster.png"
-import shop from "../../assets/shop.jpeg"
+import styles from "./ProjectsStyles.module.css";
+import viberr from "../../assets/hotelwala..png";
+import freshBurger from "../../assets/conact.png";
+import hipsster from "../../assets/quizzy.png";
+import movieapp from "../../assets/folio.png";
+import ProjectCard from "../../common/ProjectCard";
+import Devshare from "../../assets/DevCaster.png";
+import shop from "../../assets/shop.jpeg";
+import camera from "../../assets/camera.webp";
+import lens from "../../assets/cam1.png";
+import lens2 from "../../assets/cam2.png";
+
+
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
-      <ProjectCard
+        <ProjectCard
+          src={lens2}
+          link="https://github.com/mohandasss/lensBox"
+          h3="LensBox"
+          p="Click & Rent!"
+        />
+        <ProjectCard
           src={shop}
           link="https://github.com/mohandasss/shopkaro"
           h3="Shopkaro"
-          p="shopping abb gharse"
+          p="Shopping, Ghar Se!"
         />
         <ProjectCard
           src={viberr}
           link="https://github.com/mohandasss/Hotel-booking-system/tree/main/frontend"
           h3="Mohan Hotels"
-          p="Hotel Booking App"
+          p="Book, Relax, Enjoy!"
         />
         <ProjectCard
           src={movieapp}
           link="https://filmfoliooo.netlify.app/"
           h3="Film Folio"
-          p="Movie Insights Platform"
+          p="Movie World, Simplified!"
         />
-         <ProjectCard
-        src={Devshare}
-        link="https://github.com/mohandasss/Realtime-Code-Sharing.git"
-        h3="Dev Caster"
-        p="Realtime Code Sharing Tool"
-      />
+        <ProjectCard
+          src={Devshare}
+          link="https://github.com/mohandasss/Realtime-Code-Sharing.git"
+          h3="Dev Caster"
+          p="Code Together, Anytime!"
+        />
         <ProjectCard
           src={freshBurger}
           link="https://github.com/mohandasss/Contact_App"
           h3="Snapsync Contacts"
-          p="Contact Manager App"
+          p="Organize, Sync, Connect!"
         />
         <ProjectCard
           src={hipsster}
           link="https://github.com/mohandasss/quizy"
           h3="Quizilla"
-          p="Quiz App"
+          p="Test Your Knowledge!"
         />
-
       </div>
     </section>
   );
